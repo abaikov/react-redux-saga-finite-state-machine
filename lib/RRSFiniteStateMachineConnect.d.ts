7a857865f6f8e06c21389cfb320112fc0583b8df
Andrey Baikov <bboyair@mail.ru>
2024-07-08 19:44:09 +0300

    package version

---
+++ lib/RRSFiniteStateMachineConnect.d.ts
@@ -0,0 +1,4 @@
+import * as React from 'react';
+import { IRRSFiniteStateMachineConnectProps } from './type/IRRSFiniteStateMachineConnectProps';
+export declare const RRSFiniteStateMachineConnect: <Props extends {} = {}>({ stateMachine, mapComponentPropsToMachineProps, context }: IRRSFiniteStateMachineConnectProps) => (Component: React.ComponentType<Props>) => (props: Props) => React.JSX.Element;
+//# sourceMappingURL=RRSFiniteStateMachineConnect.d.ts.map
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,6 @@
 import { IRRSFiniteStateMachineHookProps } from './type/IRRSFiniteStateMachineHookProps';
 export { RRSFiniteStateMachineContext } from './RRSFiniteStateMachineContext';
 export { RRSFiniteStateMachineContextProvider } from './RRSFiniteStateMachineContextProvider';
-export { RRSFiniteStateMachineContextConsumer } from './RRSFiniteStateMachineContextConsumer';
+export { RRSFiniteStateMachineConnect } from './RRSFiniteStateMachineConnect';
 export declare const useStateMachine: (props: IRRSFiniteStateMachineHookProps, checkProps: any[]) => void;
 //# sourceMappingURL=index.d.ts.map
